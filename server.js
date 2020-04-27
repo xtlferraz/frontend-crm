92b2376b6cef7fcfdba52517a45d0c02703c3461
Thiago <thiago.ferraz@dbccompany.com.br>
2020-04-27 14:53:38 -0300

    update server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/build/'));
 app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/build/index.html');
 });
 app.listen(port);
 
