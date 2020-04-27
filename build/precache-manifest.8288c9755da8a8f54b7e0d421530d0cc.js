b6867133de335a3ac8e3f5be184d5ab016d0234e
Thiago <thiago.ferraz@dbccompany.com.br>
2020-04-27 15:29:08 -0300

    create build

---
+++ build/precache-manifest.8288c9755da8a8f54b7e0d421530d0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d9919e1dd196fbe1539f4ed52c1d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4ca9695b1f7879734d",
+    "url": "/static/css/main.45d23848.chunk.css"
+  },
+  {
+    "revision": "d60d645ad9f0a12e7d60",
+    "url": "/static/js/2.eeb2dafb.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.eeb2dafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4ca9695b1f7879734d",
+    "url": "/static/js/main.af670a9d.chunk.js"
+  },
+  {
+    "revision": "6ee6311eaeb938cc4f3f",
+    "url": "/static/js/runtime-main.1324f73f.js"
+  }
+]);
